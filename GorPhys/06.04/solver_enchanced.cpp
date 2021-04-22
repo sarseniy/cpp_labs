4c25155853d1634984d006d0d68e39ddb3396d03
sarseniy <senokosov.ao@phystech.edu>
2021-04-22 22:52:03 +0300

    bug fix

--- GorPhys/06.04/solver_enchanced.cpp
+++ GorPhys/06.04/solver_enchanced.cpp
@@ -26,10 +26,10 @@ protected:
 	double c;
 };
 
-class CIR: public Scheme
+class CIR : public Scheme
 {
 public:
-	CIR(double c0, double T0, double L0 = 1, double N0 = 100)
+	CIR(double c0, double T0, double L0 = 1, double N0 = 1000)
 	{
 		c = c0;
 		T = T0;
@@ -56,17 +56,14 @@ public:
 		tmp[0] = U0;
 		tmp[1] = U0;
 
-		int t_count = 1;
-		for (double current_t = t; current_t <= T; current_t += t)
+		int t_count = 0;
+		for (int t1 = 1; t1 < T / t; t1++)
 		{
-			int x_count = 0;
-			for (double x = 0; x <= L; x += h)
+			for (double x = 0; x < L / h; x++)
 			{
-				tmp[t_count % 2][x_count] = tmp[(t_count - 1) % 2][x_count] - (c * t / h) *
-					(tmp[(t_count - 1) % 2][x_count] - ((x_count == 0) ? 0 : tmp[(t_count - 1) % 2][x_count - 1]));
-				x_count++;
+				tmp[t1 % 2][x] = tmp[(t1 - 1) % 2][x] - (c * t / h) *
+					(tmp[(t1 - 1) % 2][x] - ((x == 0) ? 0 : tmp[(t1 - 1) % 2][x - 1]));
 			}
-			print(tmp[t_count % 2]);
 			t_count++;
 		}
 		result = tmp[(t_count - 1) % 2];
@@ -88,4 +85,6 @@ int main() {
 
 	vector<double> a;
 	a = solver.GetResult();
+
+	return 0;
 }
